3081e443c10c6c7ff587dee5ed88f2e42b979824
shikano <pegtar377512@gmail.com>
2025-01-14 21:54:10 +0900

    feat: Add Header

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,25 +1,43 @@
-import React from "react";
-import { Bars3Icon } from "@heroicons/react/24/outline";
-import { Button } from "@/components/ui/button";
+"use client";
 
-interface HeaderProps {
+import React from "react";
+import { DrawerMenu } from "@/components/DrawerMenu";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useAuthStore } from "@/store/useAuthStore";
+import {
+  ArrowLeftEndOnRectangleIcon,
+  ArrowLeftStartOnRectangleIcon,
+} from "@heroicons/react/24/solid";
+
+type HeaderProps = {
   toggleSidebar: () => void;
-}
+};
 
 export function Header({ toggleSidebar }: HeaderProps) {
+  const { isLoggedIn } = useAuthStore();
+
   return (
-    <header className="z-10 flex items-center w-full h-16 bg-background px-8 border">
+    <header className="sticky top-0 z-10 flex justify-between items-center w-full py-4 px-10 border bg-white/70 backdrop-blur-md">
       <div className="flex items-center">
-        <Button
-          variant="outline"
-          size="icon"
-          className="mr-8"
-          onClick={toggleSidebar}
-        >
-          <Bars3Icon className="" />
-        </Button>
-        <h1 className="text-4xl font-bold">Readers</h1>
+        <DrawerMenu toggleSidebar={toggleSidebar} />
+        <Link href="/">
+          <h1 className="text-4xl font-bold">Readers</h1>
+        </Link>
       </div>
+      {isLoggedIn ? (
+        <Link href="/logout">
+          <Button variant="outline">
+            <ArrowLeftStartOnRectangleIcon className="w-6 h-6" />
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/login">
+          <Button variant="outline">
+            <ArrowLeftEndOnRectangleIcon className="w-6 h-6" />
+          </Button>
+        </Link>
+      )}
     </header>
   );
 }
