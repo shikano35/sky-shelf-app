c7e42829e2aa8cf54d571e1376e09693c52a6f1f
shikano <pegtar377512@gmail.com>
2025-01-14 21:53:50 +0900

    feat: Add Footer

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -1,9 +1,15 @@
+import { Container } from "@/components/Container";
 import React from "react";
 
 export function Footer() {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <footer>
+      <Container>
+        <hr />
+        <div className="mt-16 mb-8 text-center text-gray-600 text-sm">
+          <p>&copy; {new Date().getFullYear()} Readers.</p>
+        </div>
+      </Container>
+    </footer>
   );
 }
