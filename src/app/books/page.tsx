7cf0ae735b86f590666759811d00369f7f3d669a
shikano <pegtar377512@gmail.com>
2025-01-14 21:36:03 +0900

    refactor: Split into separate components

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Container } from "@/components/Container";
+import { fetchBooks } from "@/lib/fetch";
+import { ItemCardList, Item } from "@/components/ItemCardList";
+import { Book } from "@/lib/types";
 
-export default function BooksPage() {
+export default async function BooksPage() {
+  const books: Book[] = await fetchBooks();
+
+  const items: Item[] = books.map((book) => ({
+    id: book.id,
+    name: book.name,
+    comment: book.comment,
+    imageUrl: book.imageUrl,
+    details: [
+      `著者: ${book.writer}`,
+      `ジャンル: ${book.genre}`,
+      `出版年: ${book.year}`,
+    ],
+    favoriteLink: `/books/favorites/${book.id}`,
+    detailLink: `/books/${book.id}`,
+  }));
+
   return (
-    <div>
-      <h1>Books</h1>
-    </div>
+    <Container>
+      <ItemCardList items={items} heading="書籍一覧" />
+    </Container>
   );
 }
