a2d849565885b19439c37d320199dc2576e34f8a
shikano <pegtar377512@gmail.com>
2025-01-14 21:30:36 +0900

    feat: Add basic card functionality

--- src/app/authors/[slug]/page.tsx
+++ src/app/authors/[slug]/page.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { fetchNovelists } from "@/lib/fetch";
+import { Novelist } from "@/lib/types";
+import { DetailCard } from "@/components/DetailCard";
 
-export default function AuthorsDetailsPage() {
+export default async function AuthorDetailPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const novelists: Novelist[] = await fetchNovelists();
+  const novelist = novelists.find((n) => n.id.toString() === slug);
+
+  if (!novelist) {
+    return <p>小説家が見つかりませんでした。</p>;
+  }
+
   return (
-    <div>
-      <h1>AuthorsDetails</h1>
-    </div>
+    <DetailCard
+      imageUrl={novelist.imageUrl}
+      name={novelist.name}
+      description={novelist.comment}
+      additionalInfo={
+        <>
+          <p>
+            <strong>書籍情報:</strong> {novelist.books}
+          </p>
+          <p>
+            <strong>年代:</strong> {novelist.years}
+          </p>
+        </>
+      }
+      backLink="/authors"
+      actionLink={novelist.url}
+    />
   );
 }
