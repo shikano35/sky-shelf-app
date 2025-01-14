01b3dc91949adf3610f2cc137f31fbedd9f0e81a
shikano <pegtar377512@gmail.com>
2025-01-14 22:01:36 +0900

    feat: add MenuItems components

---
+++ src/components/MenuItems/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+
+const items = [
+  { title: "トップページ", url: "/" },
+  { title: "書籍一覧", url: "/books" },
+  { title: "小説家一覧", url: "/authors" },
+  { title: "お気に入り書籍一覧", url: "/books/favorites" },
+  { title: "お気に入り小説家一覧", url: "/authors/favorites" },
+  { title: "おまかせ", url: "/random" },
+];
+
+type MenuItemsProps = {
+  onLinkClick: () => void;
+};
+
+export function MenuItems({ onLinkClick }: MenuItemsProps) {
+  return (
+    <div className="flex flex-col items-center p-4">
+      {items.map((item) => (
+        <Link
+          key={item.title}
+          href={item.url}
+          className="block p-2 hover:bg-input mt-2 rounded-md"
+          onClick={onLinkClick}
+        >
+          <span className="text-sm text-primary font-medium">{item.title}</span>
+        </Link>
+      ))}
+    </div>
+  );
+}
