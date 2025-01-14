4392ee9a0c01db2ed4aec39e220df8ba1bada143
shikano <pegtar377512@gmail.com>
2025-01-14 21:58:25 +0900

    feat: add regist components

---
+++ src/app/regist/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { RegistForm } from "@/components/RegistForm";
+
+export default function LoginPage() {
+  return (
+    <div className="sticky top-0">
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="flex flex-col gap-6">
+          <RegistForm />
+        </div>
+      </div>
+    </div>
+  );
+}
