1134764a6c3bbd3fb65bacdc1ba9a7b4e9e2ff04
shikano <pegtar377512@gmail.com>
2025-01-14 22:03:50 +0900

    feat: add types

---
+++ src/lib/types.ts
@@ -0,0 +1,35 @@
+export type Book = {
+  id: number;
+  comment?: string;
+  name: string; // 書籍名
+  writer: string; // 著者名
+  genre: string; // ジャンル
+  imageUrl: string; // 画像URL
+  bookUrl: string; // 書籍URL
+  year?: string; // 出版年
+};
+
+export type Novelist = {
+  id: number;
+  comment?: string;
+  name: string; // 小説家名
+  books: string; // 書籍情報
+  years: string; // 年代
+  imageUrl: string; // 画像URL
+  url: string; // 小説家のURL
+};
+
+export type User = {
+  id: number;
+  username: string; // ユーザー名
+  email: string; // メールアドレス
+  password: string; // パスワード
+  regDate: Date; // 登録日
+};
+
+export type Favorite = {
+  id: number;
+  userId: number; // ユーザーID
+  bookId?: number; // 書籍ID
+  novelistId?: number; // 小説家ID
+};
