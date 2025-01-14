544a5d8b2bbbad9ce7a8c6a970f59aeed81123d2
shikano <pegtar377512@gmail.com>
2025-01-14 21:07:12 +0900

    feat: Add routes for /auth and /favorites

--- server/api-routes/index.mjs
+++ server/api-routes/index.mjs
@@ -2,11 +2,15 @@ import express from "express";
 import booksRouter from "./books.mjs";
 import novelistsRouter from "./novelists.mjs";
 import usersRouter from "./users.mjs";
+import authRouter from "./auth.mjs";
+import favoriteRouter from "./favorites.mjs";
 
 const router = express.Router();
 
 router.use("/books", booksRouter);
 router.use("/novelists", novelistsRouter);
 router.use("/users", usersRouter);
+router.use("/auth", authRouter);
+router.use("/favorites", favoriteRouter);
 
 export default router;
