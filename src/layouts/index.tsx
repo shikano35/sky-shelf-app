3aa03394970d9518ccd8376d620813fee50a0e3c
shikano <pegtar377512@gmail.com>
2025-01-14 21:55:15 +0900

    feat: Add global layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,24 +5,32 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { AppSidebar } from "@/components/AppSidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const queryClient = new QueryClient();
 
   const toggleSidebar = () => {
     setIsSidebarOpen((prev) => !prev);
   };
 
   return (
-    <div className="flex flex-col">
-      <Header toggleSidebar={toggleSidebar} />
-      <SidebarProvider>
-        <AppSidebar isOpen={isSidebarOpen} />
-      </SidebarProvider>
-      <div className="absolute inset-0 z-0">
-        <main className="h-full w-full mt-16">{children}</main>
+    <QueryClientProvider client={queryClient}>
+      <div className="flex flex-col min-h-screen bg-muted">
+        <Header toggleSidebar={toggleSidebar} />
+        <SidebarProvider>
+          <div className="flex flex-1">
+            <AppSidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+            <main
+              className={`flex-1 transition-all duration-300 ${isSidebarOpen ? "md:-ml-0" : "md:-ml-64"}`}
+            >
+              {children}
+            </main>
+          </div>
+        </SidebarProvider>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </QueryClientProvider>
   );
 }
