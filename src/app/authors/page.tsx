e90385b571c2717210c4f79d47c1c953421a2c56
shikano <pegtar377512@gmail.com>
2025-01-14 21:35:45 +0900

    refactor: Split into separate components

--- src/app/authors/page.tsx
+++ src/app/authors/page.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Container } from "@/components/Container";
+import { fetchNovelists } from "@/lib/fetch";
+import { ItemCardList, Item } from "@/components/ItemCardList";
+import { Novelist } from "@/lib/types";
 
-export default function AuthorsPage() {
+export default async function AuthorsPage() {
+  const novelists: Novelist[] = await fetchNovelists();
+
+  const items: Item[] = novelists.map((novelist) => ({
+    id: novelist.id,
+    name: novelist.name,
+    comment: novelist.comment,
+    imageUrl: novelist.imageUrl,
+    details: [novelist.books, novelist.years],
+    favoriteLink: `/authors/favorites/${novelist.id}`,
+    detailLink: `/authors/${novelist.id}`,
+  }));
+
   return (
-    <div>
-      <h1>Authors</h1>
-    </div>
+    <Container>
+      <ItemCardList items={items} heading="小説家一覧" />
+    </Container>
   );
 }
