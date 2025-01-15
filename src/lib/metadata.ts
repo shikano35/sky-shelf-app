a735e1e4953d1349c11c96220929878d7572ad3b
shikano <pegtar377512@gmail.com>
2025-01-15 15:48:38 +0900

    chore: update application name

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,32 +1,32 @@
 import { Metadata } from "next";
 
 export const baseMetadata: Metadata = {
-  title: "Readers",
+  title: "SkyShelf",
   description: "青空文庫の作品を心地よく読むためのアプリケーションです。",
-  keywords: "Readers, 青空文庫, Aozora Bunko",
+  keywords: "SkyShelf, 青空文庫, Aozora Bunko",
   authors: [{ name: "Shin Takemura", url: "https://example.com" }],
   generator:
     "Node.js, Express.js JavaScript, Prisma, SQLite, Next.js, TypeScript, Tailwind CSS, Shadcn/ui, ESLint, Prettier",
-  applicationName: "Readers",
+  applicationName: "SkyShelf",
   openGraph: {
-    title: "Readers",
+    title: "SkyShelf",
     description: "青空文庫の作品を心地よく読むためのアプリケーションです。",
     type: "website",
     url: "https://example.com",
-    siteName: "Readers",
+    siteName: "SkyShelf",
     locale: "ja_JP",
     images: [
       {
         url: "https://example.com",
         width: 1200,
         height: 630,
-        alt: "Readers",
+        alt: "SkyShelf",
       },
     ],
   },
   twitter: {
     card: "summary",
-    title: "Readers",
+    title: "SkyShelf",
     description: "青空文庫の作品を心地よく読むためのアプリケーションです。",
   },
 };
