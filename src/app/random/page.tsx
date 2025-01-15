ddd96c8589b35b9b2898da5d38bcf9fda3fe55c0
shikano <pegtar377512@gmail.com>
2025-01-15 15:53:14 +0900

    feat: add random page

--- src/app/random/page.tsx
+++ src/app/random/page.tsx
@@ -1,9 +1,30 @@
+"use client";
+
 import React from "react";
+import { Container } from "@/components/Container";
+import { ItemCardList, Item } from "@/components/ItemCardList";
+import { useRandomBooks } from "@/hooks/useRandomBooks";
 
 export default function RandomBooksPage() {
+  const books = useRandomBooks();
+
+  const items: Item[] = books.map((book) => ({
+    id: book.id,
+    name: book.name,
+    comment: book.comment ?? "コメントがありません",
+    imageUrl: book.imageUrl,
+    details: [
+      `著者: ${book.writer}`,
+      `ジャンル: ${book.genre}`,
+      `出版年: ${book.year}`,
+    ],
+    detailLink: `/books/${book.id}`,
+    type: "book",
+  }));
+
   return (
-    <div>
-      <h1>RandomBooks</h1>
-    </div>
+    <Container>
+      <ItemCardList items={items} heading="おすすめ書籍一覧" />
+    </Container>
   );
 }
