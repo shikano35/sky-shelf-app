447bf5b6c6493ae4db333d85f282a9f6e48f0400
shikano <pegtar377512@gmail.com>
2025-01-15 13:56:52 +0900

    chore: Remove to create a new route page

--- src/app/authors/favorites/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FavoritesAuthorsDetailsPage() {
-  return (
-    <div>
-      <h1>FavoritesAuthorsDetails</h1>
-    </div>
-  );
-}
--- src/app/books/favorites/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FavoritesBooksDitailsPage() {
-  return (
-    <div>
-      <h1>FavoritesBooksDitails</h1>
-    </div>
-  );
-}
