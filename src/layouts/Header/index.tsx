0b77f827204e50ba83c5e85be0205f69d3ba79b5
shikano <pegtar377512@gmail.com>
2025-01-15 22:35:11 +0900

    feat: add admin button

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -5,17 +5,14 @@ import { DrawerMenu } from "@/components/DrawerMenu";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useAuthStore } from "@/store/useAuthStore";
-import {
-  ArrowLeftEndOnRectangleIcon,
-  ArrowLeftStartOnRectangleIcon,
-} from "@heroicons/react/24/solid";
+import { PlusIcon } from "@heroicons/react/24/outline";
 
 type HeaderProps = {
   toggleSidebar: () => void;
 };
 
 export function Header({ toggleSidebar }: HeaderProps) {
-  const { isLoggedIn } = useAuthStore();
+  const { isLoggedIn, isAdmin } = useAuthStore();
 
   return (
     <header className="sticky top-0 z-10 flex justify-between items-center w-full py-4 px-10 border bg-white/70 backdrop-blur-md">
@@ -25,19 +22,24 @@ export function Header({ toggleSidebar }: HeaderProps) {
           <h1 className="text-4xl font-bold">SkyShelf</h1>
         </Link>
       </div>
-      {isLoggedIn ? (
-        <Link href="/logout">
-          <Button variant="outline">
-            <ArrowLeftStartOnRectangleIcon className="w-6 h-6" />
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/login">
-          <Button variant="outline">
-            <ArrowLeftEndOnRectangleIcon className="w-6 h-6" />
-          </Button>
-        </Link>
-      )}
+      <div className="flex items-center">
+        {isAdmin && (
+          <Link href="/new" className="mr-8">
+            <Button variant="outline" size="icon">
+              <PlusIcon className="w-6 h-6" />
+            </Button>
+          </Link>
+        )}
+        {isLoggedIn ? (
+          <Link href="/logout">
+            <Button variant="outline">ログアウト</Button>
+          </Link>
+        ) : (
+          <Link href="/login">
+            <Button variant="outline">ログイン</Button>
+          </Link>
+        )}
+      </div>
     </header>
   );
 }
