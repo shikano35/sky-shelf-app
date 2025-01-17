72c5659a76ffb9e79ae7763317aa4cef20be21e8
shikano <pegtar377512@gmail.com>
2025-01-18 05:34:48 +0900

    feat: Add user-related processing

--- src/app/new/[type]/page.tsx
+++ src/app/new/[type]/page.tsx
@@ -9,8 +9,9 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import AdminGuard from "@/components/AdminGuard";
 
-interface BookFormData {
+type BookFormData = {
   name: string;
   comment?: string;
   writer: string;
@@ -18,18 +19,27 @@ interface BookFormData {
   imageUrl: string;
   bookUrl: string;
   year?: string;
-}
+};
 
-interface NovelistFormData {
+type NovelistFormData = {
   name: string;
   comment?: string;
   books: string;
   years: string;
   imageUrl: string;
   url: string;
-}
+};
 
-async function createItem(type: string, data: BookFormData | NovelistFormData) {
+type UserFormData = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+async function createItem(
+  type: string,
+  data: BookFormData | NovelistFormData | UserFormData
+) {
   const response = await fetch(`http://localhost:8080/api/${type}s`, {
     method: "POST",
     headers: {
@@ -56,14 +66,16 @@ export default function NewPage() {
           bookUrl: "",
           year: "",
         }
-      : { name: "", comment: "", books: "", years: "", imageUrl: "", url: "" };
+      : type === "novelist"
+        ? { name: "", comment: "", books: "", years: "", imageUrl: "", url: "" }
+        : { username: "", email: "", password: "" };
 
-  const [formData, setFormData] = useState<BookFormData | NovelistFormData>(
-    initialFormData
-  );
+  const [formData, setFormData] = useState<
+    BookFormData | NovelistFormData | UserFormData
+  >(initialFormData);
 
   const mutation = useMutation({
-    mutationFn: (data: BookFormData | NovelistFormData) =>
+    mutationFn: (data: BookFormData | NovelistFormData | UserFormData) =>
       createItem(type, data),
     onSuccess: () => {
       router.push(`/${type}s`);
@@ -83,65 +95,86 @@ export default function NewPage() {
     mutation.mutate(formData);
   };
 
-  const renderField = (
-    name: string,
+  function renderField(
+    name: keyof BookFormData | keyof NovelistFormData | keyof UserFormData,
     placeholder: string,
     type: "input" | "textarea" = "input",
     required = false
-  ) => {
+  ) {
     const FieldComponent = type === "input" ? Input : Textarea;
+
+    const value =
+      (formData as BookFormData)[name as keyof BookFormData] ??
+      (formData as NovelistFormData)[name as keyof NovelistFormData] ??
+      (formData as UserFormData)[name as keyof UserFormData];
+
     return (
       <FieldComponent
-        name={name}
+        name={name as string}
         placeholder={placeholder}
-        value={(formData as BookFormData | NovelistFormData)[name] || ""}
+        value={value || ""}
         onChange={handleChange}
         className="mb-4"
         required={required}
       />
     );
-  };
+  }
 
   return (
-    <div className="p-6 flex justify-center">
-      <Card className="w-full max-w-md">
-        <CardHeader className="text-center">
-          <CardTitle className="text-2xl mb-2">
-            {type === "book" ? "書籍新規登録" : "小説家新規登録"}
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={handleSubmit}>
-            {renderField("name", "名前", "input", true)}
-            {renderField("comment", "コメント (任意)", "textarea")}
-            {type === "book" && (
-              <>
-                {renderField("writer", "著者", "input", true)}
-                {renderField("genre", "ジャンル", "input", true)}
-                {renderField("bookUrl", "書籍URL", "input", true)}
-                {renderField("imageUrl", "画像URL", "input", true)}
-                {renderField("year", "年 (任意)", "input")}
-              </>
-            )}
-            {type === "novelist" && (
-              <>
-                {renderField("books", "著作", "input", true)}
-                {renderField("years", "活動年", "input", true)}
-                {renderField("url", "Wikipedia URL", "input", true)}
-                {renderField("imageUrl", "画像URL", "input", true)}
-              </>
-            )}
-            <div className="flex justify-between">
-              <Button variant="link" asChild>
-                <Link href={"/new"}>戻る</Link>
-              </Button>
-              <Button type="submit" disabled={mutation.isPending}>
-                登録
-              </Button>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
+    <AdminGuard>
+      <div className="p-6 flex justify-center">
+        <Card className="w-full max-w-md">
+          <CardHeader className="text-center">
+            <CardTitle className="text-2xl mb-2">
+              {type === "book"
+                ? "書籍新規登録"
+                : type === "novelist"
+                  ? "小説家新規登録"
+                  : "ユーザー新規登録"}
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              {type === "book" && (
+                <>
+                  {renderField("name", "名前", "input", true)}
+                  {renderField("comment", "コメント (任意)", "textarea")}
+                  {renderField("writer", "著者", "input", true)}
+                  {renderField("genre", "ジャンル", "input", true)}
+                  {renderField("bookUrl", "書籍URL", "input", true)}
+                  {renderField("imageUrl", "画像URL", "input", true)}
+                  {renderField("year", "年 (任意)", "input")}
+                </>
+              )}
+              {type === "novelist" && (
+                <>
+                  {renderField("name", "名前", "input", true)}
+                  {renderField("comment", "コメント (任意)", "textarea")}
+                  {renderField("books", "著作", "input", true)}
+                  {renderField("years", "活動年", "input", true)}
+                  {renderField("url", "Wikipedia URL", "input", true)}
+                  {renderField("imageUrl", "画像URL", "input", true)}
+                </>
+              )}
+              {type === "user" && (
+                <>
+                  {renderField("username", "ユーザーネーム", "input", true)}
+                  {renderField("email", "メールアドレス", "input", true)}
+                  {renderField("password", "パスワード", "input", true)}
+                </>
+              )}
+              <div className="flex justify-between">
+                <Button variant="link" asChild>
+                  <Link href={"/new"}>戻る</Link>
+                </Button>
+                <Button type="submit" disabled={mutation.isPending}>
+                  登録
+                </Button>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </AdminGuard>
   );
 }
